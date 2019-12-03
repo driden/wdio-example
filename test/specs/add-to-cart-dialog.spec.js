78162d575d691ce78aaa900032f4d380438814ad
Ignacio Loureiro <Ignacio.Loureiro@endava.com>
2019-12-03 12:30:13 -0300

    Added dialog tests

---
+++ test/specs/add-to-cart-dialog.spec.js
@@ -0,0 +1,6 @@
+describe("'Add to cart dialog'", () => {
+  it("should close when clicking on 'Continue shopping'", () => {});
+  it("should add the product to the shopping cart", () => {});
+  it("should show say 'There is 1 item in your cart.'", () => {});
+  it("should redirect to checlout page when clicking on 'Proceed to checkout'", () => {});
+});
