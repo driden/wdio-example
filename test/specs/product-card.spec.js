252f225d5b87d9457037f9a9c2731c8f496995b1
Ignacio Loureiro <Ignacio.Loureiro@endava.com>
2019-12-03 12:20:22 -0300

    added tests for product card

---
+++ test/specs/product-card.spec.js
@@ -0,0 +1,5 @@
+describe("Product card", () => {
+  it("should have a title, a price, and an image associated", () => {});
+  it("when hovered on, it should show 'Add to Cart' and 'More' buttons", () => {});
+  it("when selecting 'Add to Cart' from the menu it should display a new dialog ", () => {});
+});
