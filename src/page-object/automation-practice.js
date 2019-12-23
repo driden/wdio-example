f1564ce1f25461c7af5a89b119037449a676fd91
driden <776982+driden@users.noreply.github.com>
2019-12-23 14:17:03 -0300

    Added a simple test for site layout

---
+++ src/page-object/automation-practice.js
@@ -0,0 +1,13 @@
+import { Header } from "./header";
+/**
+ * This class models the automation practice page.
+ */
+export class AutomationPractice {
+  constructor() {}
+  /**
+   * @function returns a new instance of the Header class
+   */
+  get getHeader() {
+    return new Header();
+  }
+}
--- test/specs/site-layout.spec.js
+++ test/specs/site-layout.spec.js
@@ -1,5 +1,12 @@
+import { AutomationPractice } from "../../src/page-object/automation-practice";
+
 describe("The site", () => {
-  it("should have a header", () => {});
+  it("should have a header", () => {
+    const site = new AutomationPractice();
+    const header = site.getHeader();
+    assert.isTrue(header.isVisible());
+  });
+
   it("should have a searchbox", () => {});
   it("should have a menu for women, dresses & t-shirts", () => {});
   it("should have a 'Best sellers' section with a list of product cards", () => {});
